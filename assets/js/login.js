58eece52f2815684b8423f29f3d91e34ed872451
wu1755757705 <1755757705@qq.com>
2020-09-15 14:00:15 +0800

    新增了index.js文件

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,22 +9,6 @@ $(function() {
     $('.login-box').show()
     $('.reg-box').hide()
   })
-
-  //从layui 获取form
-  // var form = layui.form
-  // // 通过form.verify()自定义校验规则
-  // form.verify({
-  //   pwd: [
-  //     /^[\S]{6,12}$/
-  //     ,'密码必须6到12位，且不能出现空格'
-  //   ],
-  //   repwd:function(value) {
-  //     var pwd = $('.reg-box [name=password]').val()
-  //     if(pwd !== value) {
-  //       return '两次密码输入不一致'
-  //     }
-  //   }
-  // })
   var form = layui.form
   var layer = layui.layer
   form.verify({
@@ -32,7 +16,7 @@ $(function() {
       /^[\S]{6,12}$/
         ,'密码必须6到12位，且不能出现空格'
       ],
-      repwd:function(value) {
+     function(value) {
         var pwd = $('.reg-box [name=password]').val()
         if(value !== pwd) {
           return '两次密码不一致'
@@ -46,7 +30,7 @@ $(function() {
         username :$('#form_reg  [name = username]').val(),
         password :$('#form_reg [name=password]').val()
       }
-      $.post('http://ajax.frontend.itheima.net/api/reguser',data,function(res){
+      $.post('/api/reguser',data,function(res){
         if(res.status !== 0) {
           return layer.msg(res.message)
         }
@@ -74,3 +58,60 @@ $(function() {
     })
   })
 })
+// $(function(){
+//    //点击注册账号
+//    $('#link_reg').on('click',function(){
+//       $('.login-box').hide()
+//       $('.reg-box').show()
+//    })
+//    $('#link_login').on('click',function(){
+//     $('.login-box').show()
+//     $('.reg-box').hide()
+//  })
+// //  验证文本框和密码框
+//  var form = layui.form
+//  var layer = layui.layer
+//  form.verify({
+//    pwd : [
+//        /^[\S]{6,12}$/,'密码必须6到12位,且不能出现空格'
+//    ],
+//    repwd :function(value) {
+//      var pwd = $('.reg-box [name=password]').val()
+//      if(value !== pwd) {
+//        return '两次密码不一致'
+//      }
+//    }
+//  })
+//  //监听注册表单的提交事件
+//  $('#form_reg').submit(function(e){
+//    e.preventDefault()
+//    var data = {
+//      username : $('#form_reg [name=username]').val(),
+//      password :  $('#form_reg [name=password]').val()
+//    }
+//    $.post('/api/reguser',data,function(res){
+//      if(res.status !== 0) {
+//        return layer.msg(res.message)
+//      }
+//      layer.msg('注册成功, 请登录')
+//      $('#link_login').click()
+//    })
+//  })
+//  //监听表单登录事件
+//  $('#form_login').submit(function(e){
+//    e.preventDefault()
+//    $.ajax({
+//      url :'/api/login',
+//      method: 'post',
+//      data : $(this).serialize(),
+//      success : function(res) {
+//        if(res.status !== 0) {
+//          return layer.msg('登录失败')
+//        }
+//        layer.msg('登录成功')
+//        localStorage.setItem('token')
+//        location.href ='/index.html'
+//      }
+//    })
+//  })
+// })
